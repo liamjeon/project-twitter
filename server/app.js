644ee5c7fe40369b2fcc9627d9cb762be4efa9ab
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-17 18:20:57 +0900

    필요 module 설정, router 연결 세팅

--- server/app.js
+++ server/app.js
@@ -1,1 +1,31 @@
 import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import "express-async-errors";
+import tweetsRouter from "./router/tweets.js";
+
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet());
+app.use(morgan("tiny"));
+
+app.use("/tweets", tweetsRouter);
+app.use(
+  cors({
+    origin: ["http://localhost/3000"], //해당하는 도메인에서만 데이터 접근 가능!
+    optionsSuccessStatus: 200,
+    credentials: true, //Access-Control-Allow-Credentials:true 와 동일
+  })
+);
+
+app.use((req, res, next)=>{
+    res.sendStatus(404);
+})
+
+app.use((req,res,next)=>{
+    console.error(error);
+    res.sendStatus(500);
+})
+app.listen(8080);
