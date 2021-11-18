41363f97a129cddd40465baebc8f4627562ac46b
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-18 23:34:57 +0900

    server mvc 패턴 분리

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,57 +1,25 @@
 import express from "express";
 import "express-async-errors";
+import *as tweetController from "../controller/tweet.js";
 // import * as tweetRepository from "../data/tweet.js";
 
 const router = express.Router();
 
 // GET /tweets
 // GET /tweets?username=:username
-router.get("/", (req, res, next) => {
-  const username = req.query.username;
-  const data = username
-    ? tweetRepository.getAllByUsername(username)
-    : tweetRepository.getAll();
-  res.status(200).json(data);
-});
+//경로로 들어오면 함수를 연결. 주의! 함수 호출이 아님
+router.get("/", tweetController.getTweets); 
 
 // GET /tweets/:id
-router.get("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const tweet = tweetRepository.getById(id);
-  if (tweet) {
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
-  }
-});
+router.get("/:id", tweetController.getTweet);
 
 // POST /tweeets
-router.post("/", (req, res, next) => {
-  const { text, name, username } = req.body;
-  const tweet = tweetRepository.create(text, name, username);
-  tweets = [tweet, ...tweets];
-  console.log(tweets);
-  res.status(201).json(tweet);
-});
+router.post("/", tweetController.createTweet);
 
 // PUT /tweets/:id
-router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  const text = req.body.text;
-  const tweet = tweetRepository.update(id, text);
-  if (tweet) {
-    tweet.text = text;
-    res.status(200).json(tweet);
-  } else {
-    res.status(404).json({ message: `Tweet id(${id}) not found` });
-  }
-});
+router.put("/:id", tweetController.updateTweet);
 
 // DELETE /tweets/:id
-router.delete("/:id", (req, res, next) => {
-  const id = req.params.id;
-  tweetRepository.remove(id);
-  res.sendStatus(204);
-});
+router.delete("/:id", tweetController.deleteTweet);
 
 export default router;
