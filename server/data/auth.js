1958f714dc1bde509a6b53db8c710a63a446586b
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-23 07:01:31 +0900

    sequelize를 이용하여 로그인/회원가입( auth.js )구현

--- server/data/auth.js
+++ server/data/auth.js
@@ -34,24 +34,14 @@ const User = sequelize.define(
 );
 
 export async function findByUsername(username) {
-  return db
-    .execute("SELECT * FROM users WHERE username=?", [username]) //
-    .then((result) => result[0][0]);
+  return User.findOne({ where: { username } });
 }
 
 export async function findById(id) {
-  return db
-    .execute("SELECT * FROM users WHERE id=?", [id]) //
-    .then((result) => result[0][0]);
+  return User.findByPk(id);
 }
 
 //입력받은 유저정보 + 고유한아이다(id)를 만들어서 데이터를 넣고, id를 리턴함-->token생성용
 export async function createUser(user) {
-  const { username, password, name, email, url } = user; //오브젝트 디 컨스트럭션?
-  return db
-    .execute(
-      "INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)",
-      [username, password, name, email, url]
-    )
-    .then((result) => result[0].insertId);
+  return User.create(user).then((data)=> data.dataValues.id);
 }
