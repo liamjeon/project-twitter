f7fdd364db14622a6a4419214f5a41eff233971f
liamjeon <89363456+liamjeon@users.noreply.github.com>
2021-11-23 09:33:51 +0900

    database.js 모듈화

--- server/app.js
+++ server/app.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import 'express-async-errors';
-import cors from 'cors';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import tweetsRouter from './router/tweets.js';
-import authRouter from './router/auth.js';
-import {initSocket} from './connection/socket.js'
-import {db} from './db/database.js'
-import { connectDB } from './database/database.js';
+import express from "express";
+import "express-async-errors";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import tweetsRouter from "./router/tweets.js";
+import authRouter from "./router/auth.js";
+import { initSocket } from "./connection/socket.js";
+import { db } from "./db/database.js";
+import { connectDB } from "./database/database.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
-app.use('/tweets', tweetsRouter);
-app.use('/auth', authRouter);
+app.use("/tweets", tweetsRouter);
+app.use("/auth", authRouter);
 
 app.use((req, res, next) => {
   res.sendStatus(404);
@@ -28,8 +28,9 @@ app.use((error, req, res, next) => {
   res.sendStatus(500);
 });
 
-connectDB().then((db=>{
-  console.log('init!', db);
-  const server = app.listen(8080);
-  initSocket(server); 
-})).catch(console.error);
+connectDB()
+  .then(() => {
+    const server = app.listen(8080);
+    initSocket(server);
+  })
+  .catch(console.error);
--- server/database/database.js
+++ server/database/database.js
@@ -2,9 +2,20 @@ import MongoDb from "mongodb";
 
 const url = 'mongodb+srv://twitter:5ZVNTSBMWEIN1Tlk@cluster0.rqae8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
+let db; //client에게서 받은 db를 database 안에서만 사용할 수 있는 변수
 export async function connectDB() {
   return MongoDb.MongoClient.connect(url) //mongodb연결
-    .then((client) => client.db()); //연결이되면, 연결된 client를 받아서 클라이언트에 있는 db 자체를 리턴함.
-    //promise가 리턴됨
+    .then((client) =>{
+        db = client.db(); //연결이되면, 연결된 client를 받아서 클라이언트에 있는 db 자체를 리턴함.//promise가 리턴됨
+    })
 }
 
+//사용자에 대한 collection 전달
+export function getUsers(){
+    return db.collection('users');
+}
+
+//트윗에 대한 collection 전달
+export function getTweets(){
+    return db.collection('tweets');
+}
